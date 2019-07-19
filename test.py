911a72c546f8ff263f382132de640e534d2b70b8
Marcel Schmalzl <holzkohlengrill@gmail.com>
2019-07-19 16:01:58 +0200

    added some travis + python files to play around

---
+++ test.py
@@ -0,0 +1,14 @@
+import time
+import pandas
+
+def  main():
+    print("hello "  )
+    func(1, 1)
+    print("calc done")
+    
+def func(a= 1, b):
+    return a+b
+
+    
+if "__main__" == __name__:
+    main()
